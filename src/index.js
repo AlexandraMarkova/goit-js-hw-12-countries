f73dc2f9843f32746c14c80bb3d2efd5cb586512
AlexandraMarkova <a.markova@ukr.net>
2021-02-14 23:21:09 +0200

    вносит правки по экспортам,добавляет отслеживание ошибок

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import './styles.css';
 // import './js/pnotify'
 import '@pnotify/core/dist/BrightTheme.css';
-import fetchCountries from './js/fetchCountries';
+import './js/fetchCountries';
 
 
 
-fetchCountries();
+
 
 
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,6 +1,6 @@
 import articlesTpl from '../templates/country.hbs';
 import article from '../templates/the-country.hbs';
-const debounce = require('lodash.debounce');
+import debounce from 'lodash.debounce';
 
 import {error} from '@pnotify/core';
 import '@pnotify/core/dist/PNotify.css';
@@ -24,8 +24,14 @@ function fetchCountries(searchQuery) {
 
 refs.captionRef.innerHTML = '';
 fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`)
-  .then(response => response.json())
-  .then(res => updateData(res))
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      return { error: `Server error: ${response.status}` };
+    }
+  })
+  .then(res => updateData(res));
 };
 
 function updateData(data) {
@@ -36,7 +42,7 @@ if (data.length >= 10) {
 } else if (data.length >= 2 && data.length <= 10) {
   const markup = articlesTpl(data);
   refs.captionRef.insertAdjacentHTML('beforeend', markup);
-} else if ((data.length = 1)) {
+} else if ((data.length === 1)) {
   const markup = article(data);
   refs.captionRef.insertAdjacentHTML('beforeend', markup);
 }
